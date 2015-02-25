db8c892c39e7174356a37d2b2b43a72aaa1efbbb
Justin <justin.ison@gmail.com>
2015-02-25 18:53:12 +0100

    updated readme, added more to web api example

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Selenium Camp Examples
 
+### Information
+	* https://code.google.com/p/selenium/wiki/JsonWireProtocol (Selenium API Protocol)
+	* https://saucelabs.com/resources/appium-bootcamp (Get Started with Appium)
+	
 ### Requirements 
 	* Ruby 2.0 or greater
 	* Bundler gem
 	* Mac OS (need iOS simulator)
 	* Appium ($ npm install -g appium)
+	* Chromedriver (brew install chromedriver)
 	
 ### Install
 	* $ git clone git@github.com:isonic1/selenium_camp.git
@@ -14,3 +19,6 @@
 	* $ appium (start server in seperate terminal)
 	* Enable your HTTP Web Proxy, set IP: 127.0.0.1 & Port: 8888
 	* $ rspec proxy-example.rb
+	
+### Run Selenium WEB API Example
+	$ rspec selenium-api-web-example.rb 
--- selenium-api-web-example.rb
+++ selenium-api-web-example.rb
@@ -1,4 +1,5 @@
 require 'selenium-webdriver'
+require 'httparty'
 require 'rspec'
 
 def api_page_title
@@ -17,6 +18,10 @@ def api_find(body = {}) #same as find_element
   HTTParty.post("#{@server_url}/session/#{@web.send(:bridge).session_id}/element", body: body.to_json)["value"]["ELEMENT"]
 end
 
+def api_insert_text(id, text) #same as .send_keys
+  HTTParty.post("#{@server_url}/session/#{@web.send(:bridge).session_id}/element/#{id}/value", body: { value: [text] }.to_json)
+end
+
 describe 'Web Using Selenium API' do
   
   before :each do
@@ -33,7 +38,12 @@ describe 'Web Using Selenium API' do
   it 'Navigate a Web Page with the Selenium API' do
     expect(api_page_title).to eq "Wunderlist | To-do list, Reminders, Errands - App of the Year!"
     api_click api_find(using: "link text", value: "Create a free account")
-    @wait.until { api_find(using: "link text", value: "Forgot your password?") }
-    expect(api_page_title).to eq "Sign Up: Wunderlist"
+    @wait.until { api_page_title == "Sign Up: Wunderlist" }
+    api_insert_text api_find(using: "xpath", value: "//input[@type='text']"), "Selenium Camp"
+    api_insert_text api_find(using: "xpath", value: "//input[@type='email']"), "hello@seleniumcamp.com"
+    api_insert_text api_find(using: "xpath", value: "//input[@type='password']"), "selenium"
+    api_click api_find(using: "xpath", value: "//input[@value='Create Free Account']")
+    api_find(using: "css selector", value: "div.message")
+    expect(api_get_text api_find(using: "css selector", value: "div.message")).to eq "An account with this email address already exists."
   end
 end
